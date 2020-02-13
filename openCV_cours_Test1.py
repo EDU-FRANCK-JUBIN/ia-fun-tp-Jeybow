8ace9d8938ac7a111b389f326681fe8f41a5e90d
Jeybow <jeybow@hotmail.fr>
2020-02-13 16:31:52 +0100

    Fin Test 1
    Fin Test 1

--- openCV_cours_Test1.py
+++ openCV_cours_Test1.py
@@ -10,8 +10,24 @@ import sys
 from matplotlib import pyplot as plt
 
 imagePath = r'OpenCv/image0.jpg'
-dirCascadeFiles = r'../OpenCv/haarcascades_cuda/'
+dirCascadeFiles = r'OpenCv/opencv/haarcascades_cuda/'
 cascadefile = dirCascadeFiles + "haarcascade_frontalface_default.xml"
 classCascade = cv2.CascadeClassifier(cascadefile)
 image = cv2.imread(imagePath)
+plt.imshow(image)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+plt.imshow(gray)
+faces = classCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30,30),
+    flags = cv2.CASCADE_SCALE_IMAGE
+)
+print("il y a {0} visage(s).".format(len(faces)))
+
+# Dessine des rectangles autour des visages trouvés
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x,y), (x+w, y+h), (0, 255, 0), 2)
 plt.imshow(image)
